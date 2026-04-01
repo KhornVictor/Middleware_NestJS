77820d894b01d0961f8d7f5a655af940181025df
victorkhornkh@gmail.com <victorkhornkh@gmail.com>
2026-04-01 16:22:16 +0700

    GuardCheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AuthenticationMiddleware } from './common/middleware/authentication.middleware';
 import { RequestService } from './request/request.service';
+import { AuthGuard } from './common/guards/auth.guard';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService, AuthenticationMiddleware],
+  providers: [
+    AppService,
+    RequestService,
+    AuthenticationMiddleware,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
-
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthenticationMiddleware)
-      .forRoutes('*');
+    consumer.apply(AuthenticationMiddleware).forRoutes('*');
   }
 }
